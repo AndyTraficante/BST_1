090e6fac47c2e02f37a1fd913cea18cc9832e888
AndyTraficante <andyt.0814@gmail.com>
2020-11-10 16:32:51 -0500

    Help with postorder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,7 +20,7 @@ public class Main {
         System.out.println();
         //test.preOrderTraversal(test.root);
 
-        test.inOrderTraversal((test.root));
+        test.postOrderTraversal(test.root);
 
         //System.out.println(test.root);
 
